50e190198294512c96fd949e0e5f8dc83ee7710f
Otuekong <sampsonpaul361@gmail.com>
2022-07-13 16:40:21 -0700

    function that prints a string followed by a new line to stdout

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return - Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers, I fear thhe lack of themn - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
